6f35c539a2b19e4ca5cafc4ad03962a62a1a3578
knurm <knurmberg@gmail.com>
2015-10-11 14:41:08 +0300

    Pseudocode.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,36 @@
 /**
  * Created by acer on 19.09.2015.
  */
-function addThing () {
-    var post = document.getElementById('todo_input').value;
-    document.getElementById('posts').innerHTML = post;
-    //('<li>').text(post).prependTo('#posts');
-    ('#todo_input').value = '';
+function onAddToDoClicked () {
+    addToDoToUl();
 }
+
+function addToDoToUl () {
+    var newToDo = newToDo();
+    // Prepends a newToDo to ul
+}
+
+function addDeleteButton () {
+
+    // OnClick removeToDo
+    // Returns a delete button
+}
+
+function newToDo () {
+    var deleteButton = addDeleteButton();
+    // Creates a div
+    // Takes input value
+    // Puts input value and delete button into the div
+    // Returns newToDo
+}
+
+function removeToDo (id) {
+    // Removes a ToDo
+}
+
+
+
+/*var post = document.getElementById('todo_input').value;
+ document.getElementById('posts').innerHTML = post;
+ //('<li>').text(post).prependTo('#posts');
+ ('#todo_input').value = '';*/
